7ae0f2fb914cca95932f424f0ec62455394d1df2
YUka <bibidi-babidi-boo.321@hotmail.co.jp>
2020-03-12 07:41:18 +0000

    09kadai

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::get('/', function () {
 
 Route::group(['prefix'=>'admin'],function(){
     Route::get('news/create','Admin\NewsController@add');
+    
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('admin/profile/create','Admin\ProfileController@add');
+    Route::get('admin/profile/edit','Admin\ProfileController@add');
 });
 
 /*  「http://XXXXXX.jp/XXX というアクセスが来たときに、 
